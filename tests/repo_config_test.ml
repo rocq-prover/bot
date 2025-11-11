c10913397e4ed822bbd57e328f7bc25605fd83ef
kim.ly <lykimq@gmail.com>
2025-11-11 14:30:07 +0100

    test: add test for config error cases and add property test for integer

--- tests/repo_config_test.ml
+++ tests/repo_config_test.ml
@@ -2,32 +2,7 @@ open Base
 open Bot_components
 open Repo_config
 open Alcotest
-
-(** Helper to create a testable type for repo_config.t *)
-let repo_config_testable =
-  let pp fmt config =
-    Stdlib.Format.fprintf fmt
-      "{ github_owner=%s; github_repo=%s; github_installation_id=%s; \
-       gitlab_domain=%s; org_name=%s }"
-      config.github_owner config.github_repo
-      (Option.value ~default:"None"
-         (Option.map config.github_installation_id ~f:Int.to_string) )
-      (Option.value ~default:"None" config.gitlab_domain)
-      (Option.value ~default:"None" config.org_name)
-  in
-  let equal a b =
-    String.equal a.github_owner b.github_owner
-    && String.equal a.github_repo b.github_repo
-    && Option.equal Int.equal a.github_installation_id b.github_installation_id
-    && Option.equal String.equal a.gitlab_domain b.gitlab_domain
-    && Option.equal String.equal a.gitlab_owner b.gitlab_owner
-    && Option.equal String.equal a.gitlab_repo b.gitlab_repo
-    && Option.equal Int.equal a.github_project_number b.github_project_number
-    && Option.equal String.equal a.org_name b.org_name
-    && Option.equal String.equal a.team_name b.team_name
-    && Option.equal String.equal a.minimizer_url b.minimizer_url
-  in
-  testable pp equal
+open Repo_config_test_helpers
 
 let test_minimal_config () =
   let toml_str = {|
