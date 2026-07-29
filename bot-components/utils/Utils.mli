d9bb282a78fa9bf5e93c92c914022e2f19426607
kim.ly <lykimq@gmail.com>
2026-07-29 13:25:09 +0200

    [config-schema]: add TOML helpers for nested table, ints, bools, and arrays

--- bot-components/utils/Utils.mli
+++ bot-components/utils/Utils.mli
@@ -6,6 +6,17 @@ val toml_of_file : string -> Toml.Types.table
 
 val subkey_value : Toml.Types.table -> string -> string -> string option
 
+val subkey_table :
+  Toml.Types.table -> string -> string -> Toml.Types.table option
+
+val subkey_int : Toml.Types.table -> string -> string -> int option
+
+val key_value : Toml.Types.table -> string -> string option
+
+val key_array : Toml.Types.table -> string -> string list option
+
+val key_bool : Toml.Types.table -> string -> bool option
+
 val find : string -> Toml.Types.table -> Toml.Types.value
 
 val list_table_keys : Toml.Types.table -> string list
