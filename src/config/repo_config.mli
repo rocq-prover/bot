758cd69967c3443873a4cdc0020ffaf4366f9461
kim.ly <lykimq@gmail.com>
2026-08-03 13:36:12 +0200

    [repo-config]: add job helpers and tests for it

--- src/config/repo_config.mli
+++ src/config/repo_config.mli
@@ -33,3 +33,15 @@ val backport_enabled : t -> bool
 
 val find_by_backport_project :
   install_id:int -> project_number:int -> (string, t) Base.Hashtbl.t -> t option
+
+val is_bench_job : t -> string -> bool
+
+val is_doc_artifact_job : t -> string -> bool
+
+val github_full_name : t -> string
+
+val gitlab_job_url : t -> job_id:int -> string option
+
+val gitlab_pages_artifact_url :
+  t -> job_id:int -> artifact:string -> string option
+
--- tests/test_repo_config.ml
+++ tests/test_repo_config.ml
@@ -126,6 +126,62 @@ let test_backport_enabled () =
        (Repo_config.find_by_backport_project ~install_id:102161
           ~project_number:11 without_project ) )
 
+let test_jobs_helper () =
+  let with_jobs =
+    parse
+      {|
+    [repositories.rocq]
+    github = "rocq-prover/rocq"
+    gitlab_domain = "gitlab.inria.fr"
+    gitlab_owner = "coq"
+    gitlab_repo = "coq"
+
+    [repositories.rocq.jobs]
+    bench_job = "bench"
+    use_rocq_job_status = true
+    silence_docker_manifest_errors = true
+    doc_artifact_jobs = ["doc:refman", "doc:stdlib"]
+    |}
+  in
+  let without_jobs =
+    parse {|
+  [repositories.demo]
+  github = "my-org/my-repo"
+  |}
+  in
+  let cfg_on =
+    Option.value_exn
+      (Repo_config.find_by_github ~owner:"rocq-prover" ~repo:"rocq" with_jobs)
+  in
+  let cfg_off =
+    Option.value_exn
+      (Repo_config.find_by_github ~owner:"my-org" ~repo:"my-repo" without_jobs)
+  in
+  (check bool) "bench on" true (Repo_config.is_bench_job cfg_on "bench") ;
+  (check bool) "bench wrong name" false
+    (Repo_config.is_bench_job cfg_on "build") ;
+  (check bool) "bench absent" false
+    (Repo_config.is_bench_job cfg_off "bench_job") ;
+  (check bool) "doc on" true
+    (Repo_config.is_doc_artifact_job cfg_on "doc:refman") ;
+  (check bool) "doc off" false
+    (Repo_config.is_doc_artifact_job cfg_off "doc:refman") ;
+  (check string) "gh name" "rocq-prover/rocq"
+    (Repo_config.github_full_name cfg_on) ;
+  (check (option string))
+    "job url" (Some "https://gitlab.inria.fr/coq/coq/-/jobs/42")
+    (Repo_config.gitlab_job_url cfg_on ~job_id:42) ;
+  (check (option string))
+    "pages url"
+    (Some
+       "https://coq.gitlabpages.inria.fr/-/coq/-/jobs/42/artifacts/doc/index.html"
+    )
+    (Repo_config.gitlab_pages_artifact_url cfg_on ~job_id:42
+       ~artifact:"doc/index.html" ) ;
+  (check (option string))
+    "job url missing" None
+    (Repo_config.gitlab_job_url cfg_off ~job_id:1)
+
 let () =
   run "Repo_config tests"
     [ ( "parse"
@@ -134,4 +190,5 @@ let () =
         ; ("bad github", `Quick, test_bad_github)
         ; ("missing section", `Quick, test_missing_section)
         ; ("find miss", `Quick, test_find_miss)
-        ; ("backport enabled", `Quick, test_backport_enabled) ] ) ]
+        ; ("backport enabled", `Quick, test_backport_enabled)
+        ; ("jobs", `Quick, test_jobs_helper) ] ) ]
